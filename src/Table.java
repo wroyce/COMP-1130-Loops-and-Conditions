491238f9dffdb76924dc9fcbf3c308dd7beca981
wroyce <40677919+wroyce@users.noreply.github.com>
2021-03-22 01:52:34 -0700

    Created Table + TableDriver
    TableDriver runs the main method with an object linked to Table, being the API class, of sorts.

---
+++ src/Table.java
@@ -0,0 +1,42 @@
+import java.util.concurrent.TimeUnit;
+
+public class Table {
+
+    private static int width, height;
+
+    public Table (int newWidth, int newHeight) {
+        width = newWidth;
+        height = newHeight;
+    }
+
+    public Table getNumbers() throws InterruptedException {
+        printMessage("Generating table...");
+        printMessage("Table specs: " + width + " x " + height);
+        printMessage("Table Operation: Addition");
+        printSpace();
+        addDelay(2);
+
+        int w = 1;
+        for (int i = 1; i <= height; i++ ) {
+            System.out.print(" | ");
+            for (int j = 1; j <= width; j++) {
+                System.out.print(w++ + " | ");
+            }
+            System.out.print("\n");
+        }
+        return null;
+    }
+
+    public static void printMessage (String message) {
+        System.out.println(message);
+    }
+
+    public static void printSpace() {
+        System.out.println();
+    }
+
+    public static void addDelay (int delay) throws InterruptedException {
+        TimeUnit.SECONDS.sleep(delay);
+    }
+
+}
---
+++ src/TableDriver.java
@@ -0,0 +1,7 @@
+public class TableDriver {
+
+    public static void main (String[] args) throws InterruptedException {
+        Table numbers = new Table(6, 8).getNumbers();
+    }
+
+}
