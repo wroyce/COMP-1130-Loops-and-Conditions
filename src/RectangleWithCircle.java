2936a280b2fce75e47a22a7308704a6feaef4d15
4vian5 <chhetria191@mytru.ca>
2021-03-24 04:37:16 +0545

    Create RectangleWithCircle.java

---
+++ src/RectangleWithCircle.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class RectangleWithCircle {
+	public static void main(String[] args) {		
+
+		int height = readNumber("height");
+		int width = readNumber("width");
+		
+		CreateShape(height, width);
+	}
+
+	public static int readNumber(String prompt){
+		Scanner scan = new Scanner(System.in); 	
+
+		System.out.print("Please enter " + prompt + " of the rectangle: ");
+		int value = scan.nextInt();
+		return value;
+	}
+
+	private static void CreateShape(int height, int width) {
+		for(int i = 0; i < height; i++){
+			for(int j = 0; j < width; j++){
+
+				if((int)Math.pow((i-height/2)*height, 2) + (int)Math.pow((j-width/2)*width, 2) < (int)Math.pow(height*width,2)/8){
+					if (i == height/2 && j == width/2){
+					System.out.print(" ");
+					}
+					else{
+						System.out.print("0");
+					}
+			}
+				else{
+				System.out.print("#");
+				}
+			}
+		System.out.println("");
+		}
+	}
+
+}
